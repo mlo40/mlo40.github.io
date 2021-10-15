dbcb12b2c44e1c471b815179d8b39dcc7dd3bc6b
emlo40 <emils.gailis@gmail.com>
2021-10-15 19:41:12 +0300

    added

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.write(`
 <div class="item1 cwhite">
 	<a href="https://emlo40.com" style="text-decoration: none; color:green;" class="button"><h1 style="font-family:'raster';">Emlos bad site</h1></a>
 	<div class="btn-group">
-		<a href="https://emlo40.com/21-2/" class="button">info</a>
+		<a href="info.html" class="button">info</a>
 		<a href="https://emlo40.gay" class="button">nice</a>
 	</div>
 </div>

33f5dedba9e7d56c0a516a0ff091db7c0e3b28ab
TheEnderCore <theendercore@gmail.com>
2021-12-22 19:58:38 +0200

    returing things back

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ document.write(`
 <p>Welcome to my work in progress bs site</p>
 <p>My channal</p>
 <a href="https://www.youtube.com/channel/UCcJ1_9wz9yy3OR_u2mswS3A"/><img class="social" src="images/avatars/icon3.jpeg" style="width: 100px; height: 100px;"></a>
-<p style="">My palls pp </p>
+<p style="">My palls</p>
 <a href="https://www.youtube.com/channel/UCssyI6aO01o0d2OZ8c-rZkA"/><img class="social" src="images/avatars/icon.jpeg" style=" width: 100px; height: 100px;"></a>
 <a href="https://www.twitch.tv/tviron41"/><img class="social" src="images/avatars/icon2.jpeg" style=" width: 100px; height: 100px;"></a>
 `);

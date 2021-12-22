51f1412010f15b38d27bb870204b8ab12612a104
emlo40 <emils.gailis1@gmail.com>
2021-12-22 18:56:00 +0200

    unused

--- bot/js/content2.js
+++
@@ -1,6 +0,0 @@
-document.write(`
-<ul class="sbtn">
- <li><button class="tablinks" onclick="openpanel(event, 'Main')">Main</button></li>
- <li><button class="tablinks" onclick="openpanel(event, 'Other')">Other</button></li>
-</ul>
-`);
--- bot/js/header.js
+++
@@ -1,8 +0,0 @@
-document.write(`
-<div class="btn-group item1 bord">
-<a href="index.html" class="button">Main</a>
-<a href="policy.html" class="button">Policy</a>
-<a href="terms.html" class="button">Terms && services</a>
-<a href="dash.html" class="button">Dash</a>
-</div>
-`);

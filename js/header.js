dd1793657183cacfa4a3207bab3746e138f71739
TheEnderCore <theendercore@gmail.com>
2021-12-22 20:36:20 +0200

    refactorization - pt 1

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 document.write(`
 <div class="item1 cwhite">
-	<a href="https://emlo40.com" style="text-decoration: none; color:green;" class="button"><h1 style="font-family:'raster';">Emlos bad site</h1></a>
+	<a href="https://emlo40.com" style="text-decoration: none; color:green;" class="button"><h1>Emlos bad site</h1></a>
 	<div class="btn-group">
 		<a href="info.html" class="button">info</a>
 		<a href="https://emlo40.gay" class="button">nice</a>
